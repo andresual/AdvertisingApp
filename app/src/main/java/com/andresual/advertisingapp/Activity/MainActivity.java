7b91d9d7d134b8b7521dc59fd4943eb35233e17c
Andrean Firmansyah <andrean9103@gmail.com>
2019-05-26 04:41:01 +0700

    initial commit

--- app/src/main/java/com/andresual/advertisingapp/Activity/MainActivity.java
+++ app/src/main/java/com/andresual/advertisingapp/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.andresual.advertisingapp;
+package com.andresual.advertisingapp.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.andresual.advertisingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
