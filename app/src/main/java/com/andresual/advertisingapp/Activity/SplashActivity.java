b6281c6a5775804028060f252d87522cf6712787
Andrean Firmansyah <andrean9103@gmail.com>
2019-05-27 01:54:06 +0700

    Splash activity

---
+++ app/src/main/java/com/andresual/advertisingapp/Activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.andresual.advertisingapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andresual.advertisingapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
